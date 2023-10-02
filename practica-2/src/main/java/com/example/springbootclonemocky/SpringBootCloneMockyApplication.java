1cf8230d774886263320798c9760125d0de38707
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-10-02 15:31:00 -0400

    creando proyecto

---
+++ practica-2/src/main/java/com/example/springbootclonemocky/SpringBootCloneMockyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootclonemocky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloneMockyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCloneMockyApplication.class, args);
+    }
+
+}
