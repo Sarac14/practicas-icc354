62d1b82b098f1b7b3de5c3b639297efe593258f3
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-10-05 08:55:13 -0400

    .

--- practica-2/src/main/java/com/example/springbootclonemocky/config/SecurityConfig.java
+++ practica-2/src/main/java/com/example/springbootclonemocky/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
     public SecurityConfig(SeguridadServices seguridadServices, PasswordEncoder passwordEncoder) {
         this.seguridadServices = seguridadServices;
         this.passwordEncoder = passwordEncoder;
+
     }
 
     @Bean
